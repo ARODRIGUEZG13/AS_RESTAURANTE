9fd93abad42f7e137320a326532ccf2db6ca7d51
Aniversario3 <Aniversario3@10.1.0.134>
2020-04-04 07:04:50 -0600

    Cambio de usuario y contraseña bd

--- src/java/Controlador/Conexion.java
+++ src/java/Controlador/Conexion.java
@@ -9,8 +9,8 @@ import java.sql.Statement;
 public class Conexion {
     
     private Connection con;
-    private String Username = "ARODRIGUEZ";
-    private String Password = "alex1997";
+    private String Username = "EGOMEZ";
+    private String Password = "123";
     private String Puerto   = "1521";
     private String Hostname = "@db-restaurante.cst6mnzu82my.us-east-2.rds.amazonaws.com";
     private String Clasname = "oracle.jdbc.driver.OracleDriver";
